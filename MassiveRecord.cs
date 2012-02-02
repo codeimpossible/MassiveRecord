3ef41c7457ec8855cba1865853e49fb42fea1b32
Jared Barboza <jared.m.barboza@gmail.com>
2012-02-01 22:26:51 -0500

    Initial Commit

---
+++ MassiveRecord.cs
@@ -0,0 +1,82 @@
+﻿namespace MassiveRecord {
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    using Massive;
+    
+    public class MassiveContextBase : DynamicModel {
+        private List<Action<dynamic>> before_save_filters = null;
+
+        public MassiveContextBase ( string connectionStringName, List<Action<dynamic>> filters = null ) :
+            base( connectionStringName ) {
+            before_save_filters = filters ?? new List<Action<dynamic>>();
+        }
+
+        public void RegisterBeforeSaveFilter ( Action<dynamic> filter ) {
+            before_save_filters.Add( filter );
+        }
+
+        public override bool BeforeSave ( dynamic item ) {
+            before_save_filters.ForEach( a => a( item ) );
+            return true;
+        }
+
+        public override bool TryInvokeMember ( System.Dynamic.InvokeMemberBinder binder, object[] args, out object result ) {
+            if( binder.Name.ToLower().StartsWith( "findby" ) ) {
+                var where = new StringBuilder();
+                var method = binder.Name.ToLower().Replace( "findby", "" );
+                var methodColumns = Regex.Split( method, "and" );
+
+                for( int i = 0; i < methodColumns.Length; i++ )
+                    where.AppendFormat( "{0} [{1}] = {2} ", i > 0 ? " and " : "", methodColumns[i], ToSql( args[i] ) );
+                result = All( where: where.ToString() );
+                return true;
+            }
+
+            var type = GetType();
+            if( type.GetMethod( binder.Name ) != null ) {
+                result = type.InvokeMember( binder.Name, BindingFlags.Default | BindingFlags.InvokeMethod, null, this, args );
+                return true;
+            }
+
+            return base.TryInvokeMember( binder, args, out result );
+        }
+
+        private string ToSql ( object p ) {
+            if( p is String ) return String.Format( "'{0}'", (string)p );
+            else if( p is DateTime ) return String.Format( "'{0}'", ( (DateTime)p ).ToString() );
+            else return p.ToString();
+        }
+    }
+
+    public static class DynamicTable {
+        private readonly static IDictionary<String, List<Action<dynamic>>> before_save_filters =
+            new Dictionary<String, List<Action<dynamic>>>();
+
+        public static dynamic Create<MODEL> ( string connectionString = null, string primaryKey = "Id" ) {
+            var table = typeof( MODEL ).Name;
+            return Create( table, connectionString, primaryKey );
+        }
+
+        public static dynamic Create ( string table, string connectionString = null, string primaryKey = "Id" ) {
+            var filters = before_save_filters.ContainsKey( table ) ? before_save_filters[table] : null;
+            return new MassiveContextBase( connectionString, filters ) {
+                TableName = table,
+                PrimaryKeyField = primaryKey
+            };
+        }
+
+        public static void RegisterBeforeSaveFilter<TMODEL> ( Action<dynamic> filter ) {
+            var type = typeof( TMODEL ).Name;
+            RegisterBeforeSaveFilter( type, filter );
+        }
+
+        public static void RegisterBeforeSaveFilter ( string tableName, Action<dynamic> filter ) {
+            if( before_save_filters.ContainsKey( tableName ) ) before_save_filters[tableName].Add( filter );
+            else before_save_filters.Add( tableName, new List<Action<dynamic>> { filter } );
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# MassiveRecord will make you love Massive even more.
+
+If you've used [Massive](http://github.com/robconery/massive), you know how awesome it is. But you've probably thought _"This still seems like a lot more code than I really want to have to write"_. I was thinking the same thing.
+
+Goals
+ * Eliminate the need to create a new class for each Massive "Table"
+ * Add awesome functionality from ActiveRecord (ex: `FindBy...` and `Reload`)
+ * Add registration for event hooks: BeforeSave, BeforeDelete
+ * Add registration for validation hooks
+ * Do all of this in as few lines of code as possible
+ * And most importantly, make Massive that much easier and fun to use!
+
+What if you could do something like this:
+
+    var contactsTable = DynamicTable.Create(
+                                    "Person.Contact",   // table
+                                    "AdventureWorks",   // connectionstring
+                                    "ContactID" );      // primarykey
+
+    var contacts = contactsTable.FindByFirstNameAndLastName( "Jay", "Adams" );
+
+Pretty clean, right? Yeah I think so too. But that's not enough, what if we wanted to have the `FirstName` capitalized every time we save a contact? Normally we'd have to override the BeforeSave method in our `Contacts` class. With MassiveRecord it takes 1 line of code:
+
+    DynamicTable.RegisterBeforeSaveFilter( "Person.Contact", user => user.FirstName = user.FirstName.ToUpper() );
+
+Yeah, it's *that* easy.
+
+
+## Installation
+
+Just download the MassiveRecord.cs file along with [Massive](http://github.com/robconery/massive) and throw them into your project and you're done!
