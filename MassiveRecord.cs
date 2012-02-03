d8143a4ffc787c28ff18cdc54caeeb03d1f448fe
Jared Barboza <jared.m.barboza@gmail.com>
2012-02-03 00:08:57 -0500

    Experimental branch: working on adding configuration with a DSL

--- MassiveRecord.cs
+++ MassiveRecord.cs
@@ -6,21 +6,32 @@
     using System.Text.RegularExpressions;
 
     using Massive;
-    
-    public class MassiveContextBase : DynamicModel {
-        private List<Action<dynamic>> before_save_filters = null;
 
-        public MassiveContextBase ( string connectionStringName, List<Action<dynamic>> filters = null ) :
+    public enum FilterType { BeforeSave, BeforeDelete }
+
+    public class MassiveContextBase : DynamicModel {
+        private IDictionary<FilterType, List<Action<dynamic>>> bound_filters = null;
+
+        public MassiveContextBase ( string connectionStringName,
+                                    IDictionary<FilterType, List<Action<dynamic>>> filters = null ) :
             base( connectionStringName ) {
-            before_save_filters = filters ?? new List<Action<dynamic>>();
+            bound_filters = filters ?? new Dictionary<FilterType, List<Action<dynamic>>> {
+                    { FilterType.BeforeSave, new List<Action<dynamic>>() },
+                    { FilterType.BeforeDelete, new List<Action<dynamic>>() }
+                };
         }
 
-        public void RegisterBeforeSaveFilter ( Action<dynamic> filter ) {
-            before_save_filters.Add( filter );
+        public void RegisterFilter ( FilterType type, Action<dynamic> filter ) {
+            bound_filters[type].Add( filter );
         }
 
         public override bool BeforeSave ( dynamic item ) {
-            before_save_filters.ForEach( a => a( item ) );
+            bound_filters[FilterType.BeforeSave].ForEach( a => a( item ) );
+            return true;
+        }
+
+        public override bool BeforeDelete ( dynamic item ) {
+            bound_filters[FilterType.BeforeDelete].ForEach( a => a( item ) );
             return true;
         }
 
@@ -53,30 +64,57 @@
     }
 
     public static class DynamicTable {
-        private readonly static IDictionary<String, List<Action<dynamic>>> before_save_filters =
-            new Dictionary<String, List<Action<dynamic>>>();
+        private readonly static IDictionary<String, IDictionary<FilterType, List<Action<dynamic>>>> filters =
+            new Dictionary<String, IDictionary<FilterType, List<Action<dynamic>>>>();
 
-        public static dynamic Create<MODEL> ( string connectionString = null, string primaryKey = "Id" ) {
-            var table = typeof( MODEL ).Name;
-            return Create( table, connectionString, primaryKey );
+        private readonly static IDictionary<String, ISettings> settings = new Dictionary<String, ISettings>();
+
+        // interfaces for our "mini DSL"
+        public interface IWhenAskedFor {
+            IUse WhenAskedFor ( string table );
+        }
+        public interface IUse {
+            ISettings Use ( Action<ISettings> use );
+        }
+        public interface ISettings {
+            string Table { get; set; }
+            string ConnectionString { get; set; }
+            string PrimaryKey { get; set; }
+        }
+
+        // configurator implements them all we just cast it around
+        public class DynamicTableConfigurator : IWhenAskedFor, IUse, ISettings {
+            public IUse WhenAskedFor ( string table ) { return this; }
+
+            public ISettings Use ( Action<ISettings> use ) {
+                use(this);
+                return this;
+            }
+
+            public string Table { get; set; }
+            public string ConnectionString { get; set; }
+            public string PrimaryKey { get; set; }
+        }
+
+        public static void Configure ( Func<IWhenAskedFor, ISettings> config ) {
+            var tableSettings = config(new DynamicTableConfigurator());
+            settings.Add( tableSettings.Table, tableSettings );
         }
 
         public static dynamic Create ( string table, string connectionString = null, string primaryKey = "Id" ) {
-            var filters = before_save_filters.ContainsKey( table ) ? before_save_filters[table] : null;
-            return new MassiveContextBase( connectionString, filters ) {
+            var table_filters = filters.ContainsKey( table ) ? filters[table] : null;
+            return new MassiveContextBase( connectionString, table_filters ) {
                 TableName = table,
                 PrimaryKeyField = primaryKey
             };
         }
 
-        public static void RegisterBeforeSaveFilter<TMODEL> ( Action<dynamic> filter ) {
-            var type = typeof( TMODEL ).Name;
-            RegisterBeforeSaveFilter( type, filter );
-        }
-
-        public static void RegisterBeforeSaveFilter ( string tableName, Action<dynamic> filter ) {
-            if( before_save_filters.ContainsKey( tableName ) ) before_save_filters[tableName].Add( filter );
-            else before_save_filters.Add( tableName, new List<Action<dynamic>> { filter } );
+        public static void RegisterFilter ( FilterType type, string tableName, Action<dynamic> filter ) {
+            if( filters.ContainsKey( tableName ) ) {
+                if( filters[tableName].ContainsKey( type ) ) {
+                    filters[tableName][type].Add( filter );
+                } else filters[tableName].Add( type, new List<Action<dynamic>>() );
+            } else filters.Add( tableName, new Dictionary<FilterType, List<Action<dynamic>>> { { type, new List<Action<dynamic>> { filter } } } );
         }
     }
 }
